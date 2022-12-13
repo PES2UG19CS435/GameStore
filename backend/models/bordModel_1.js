c3097ee462e0718b4893137d05e7d43da1526cd6
PES2UG19CS435 <75541885+PES2UG19CS435@users.noreply.github.com>
2022-12-13 16:03:46 +0530

    Delete bordModel_1.js

--- backend/models/bordModel_1.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const bordSchema=new mongoose.Schema({
-    cardno:{type:String,required:true,unique:true,dropDups:true},
-    pin:{type:String,required:true,unique:true},
-    cvv:{type:String,required:true,unique:true},
-    isAuth:{type:Boolean,required:true,default:false}
-});
-const bordModel=mongoose.model("Bord",bordSchema);
-export default bordModel;
